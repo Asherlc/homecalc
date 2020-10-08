96889b6052d12f3af8721a373806f56b5286da0c
Asher Cohen <asherlc@asherlc.com>
2020-10-08 15:41:02 -0700

    wip

--- src/components/TimeChart.tsx
+++ src/components/TimeChart.tsx
@@ -1,3 +1,4 @@
+import numeral from "numeral";
 import {
   ComposedChart,
   Line,
@@ -11,19 +12,63 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-import { addYears, eachMonthOfInterval, format } from "date-fns";
+import { eachMonthOfInterval, format, startOfMonth } from "date-fns";
 import { colors, Paper, CircularProgress } from "@material-ui/core";
 import { useIssues } from "../hooks/useIssues";
+import { groupBy, last, sortBy } from "lodash";
+import { Issue } from "../models/Issue";
 
-const today = new Date();
-const inOneYear = addYears(today, 1);
-export const monthsFromToday = eachMonthOfInterval({
-  start: today,
-  end: inOneYear,
-});
+interface ChartDataPoint {
+  date: number;
+  [nameValue: string]: number;
+}
+
+class ChartData {
+  issues: Issue[];
+
+  constructor(issues: Issue[]) {
+    this.issues = issues;
+  }
+
+  private get issuesByMonth() {
+    return groupBy(this.issues, (issue) => {
+      return startOfMonth(issue.requiredInDate as Date).getTime();
+    });
+  }
+
+  get points(): ChartDataPoint[] {
+    const issuesByMonth = this.issuesByMonth;
+
+    return Object.entries(issuesByMonth).map(([month, issues]) => {
+      const monthDate = new Date(parseInt(month));
+
+      return issues.reduce(
+        (point, issue) => {
+          return { ...point, [`cost:${issue.name}`]: issue.buyerCost };
+        },
+        {
+          date: monthDate.getTime(),
+        }
+      );
+    });
+  }
+
+  get ticks(): number[] {
+    const timeSortedPoints = sortBy(this.points, "date");
+
+    const earliestPoint = timeSortedPoints[0];
+    const latestPoint = last(timeSortedPoints) || timeSortedPoints[0];
+    const monthsToChart = eachMonthOfInterval({
+      start: earliestPoint.date,
+      end: latestPoint.date,
+    });
+
+    return monthsToChart.map((date) => date.getTime());
+  }
+}
 
 function getColor(index: number): string {
-  const colorArray = Object.values(colors).map((color) => (color as any)[300]);
+  const colorArray = Object.values(colors).map((color) => (color as any)[500]);
   return colorArray[index % colorArray.length];
 }
 
@@ -34,45 +79,43 @@ export function TimeChart() {
     return <CircularProgress />;
   }
 
-  const chartData = monthsFromToday.map((date) => {
-    return issues
-      .filter((issue) => {
-        return issue.requiredInDate?.getMonth() === date.getMonth();
-      })
-      .reduce(
-        (hash, issue) => {
-          return {
-            ...hash,
-            [issue.name]: issue.cost,
-          };
-        },
-        {
-          name: format(date, "MMM"),
-        }
-      );
-  });
+  const chartData = new ChartData(issues);
 
   return (
     <Paper>
       <ResponsiveContainer height={500} width="100%">
-        <ComposedChart data={chartData}>
+        <ComposedChart data={chartData.points}>
           <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
+          <XAxis
+            dataKey={(point) => point.date}
+            ticks={chartData.ticks}
+            type="number"
+            domain={["auto", "auto"]}
+            tickFormatter={(tick: number) => {
+              return format(tick, "yyyy-MMM");
+            }}
+          />
+          <YAxis name="amount" allowDuplicatedCategory={false} />
+          <Tooltip
+            formatter={(amount) => {
+              return numeral(amount).format("$0,0");
+            }}
+            labelFormatter={(date) => format(new Date(date), "yyyy-MMM")}
+          />
           <Legend />
           <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-          {issues.map((issue, index) => {
-            return (
-              <Bar
-                key={issue.id}
-                dataKey={issue.name}
-                barSize={20}
-                fill={getColor(index)}
-                stackId="a"
-              />
-            );
-          })}
+          {issues.map((issue, index) => (
+            <Bar
+              key={issue.id}
+              name={issue.name}
+              dataKey={(point) => {
+                return point[`cost:${issue.name}`] ?? 0;
+              }}
+              barSize={60}
+              stackId={"a"}
+              fill={getColor(index)}
+            />
+          ))}
           <Line type="monotone" dataKey="uv" stroke="#ff7300" />
           {/* <Scatter dataKey="cnt" fill="red" /> */}
         </ComposedChart>
